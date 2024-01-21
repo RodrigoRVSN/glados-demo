278166247e53d4bce6c5c4ee9cc186ff26401b17
rodrigorvsn <rodrigovictor81@gmail.com>
2024-01-21 13:27:52 -0300

    feat: add animations to iddle, walk and running

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,27 +3,31 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private float rotationSpeed = 10f;
+    private Animator animator;
+    private float defaultSpeed = 5f;
+    private float runningMultiplier = 2f;
 
     void Start ()
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+        animator = GetComponent<Animator>();
     }
 
     float GetSpeed()
     {
-        float moveSpeed = 5f;
-
         if (Input.GetKey(KeyCode.LeftShift))
         {
-            return moveSpeed * 2;
+            return defaultSpeed * runningMultiplier;
         }
 
-        return moveSpeed;
+        return defaultSpeed;
     }
 
     void Update()
     {
+        animator.SetBool("isMoving", false);
+        animator.SetBool("isRunning", false);
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         float moveSpeed = GetSpeed();
@@ -33,11 +37,17 @@ public class PlayerMovement : MonoBehaviour
 
         transform.Translate(moveDirection * Time.deltaTime * moveSpeed, Space.World);
 
+
         if (moveDirection != Vector3.zero)
         {
+            animator.SetBool("isMoving", true);
+            if (moveSpeed > defaultSpeed)
+            {
+                animator.SetBool("isRunning", true);
+            }
+
             Quaternion toRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
             transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
         }
-
     }
 }
